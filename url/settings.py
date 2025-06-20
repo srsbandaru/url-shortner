13c7881ab11a6b78a2ace6657d09cb5cf9b39815
srsbandaru <86214783+srsbandaru@users.noreply.github.com>
2025-06-20 15:47:17 +1000

    Shortner app
    Configure the Shortner application and templates directory

--- url/settings.py
+++ url/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_bootstrap5",
+    "shortner.apps.ShortnerConfig"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = "url.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
